1f08f44b0d134808324268556ef8dd44a406d45e
ethanbtlr <4589948+ethanbtlr@users.noreply.github.com>
2023-04-27 14:11:46 -0400

    Added a reference

--- runanki.py
+++ runanki.py
@@ -1,6 +1,8 @@
 #!usr/bin/env python3
 # Copyright: Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; https://www.gnu.org/licenses/agpl.html
+# Found and adapted from: https://www.youtube.com/watch?v=c1BBPiN5ZKA&t=10s
+
 import os
 import aqt
 
