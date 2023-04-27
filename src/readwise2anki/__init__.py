c118e189346f9da6d4526605fcfca7782b70c62f
ethanbtlr <4589948+ethanbtlr@users.noreply.github.com>
2023-04-27 14:09:00 -0400

    Added an addon option to the menu. Showed the addon window when the option is clicked.

--- src/readwise2anki/__init__.py
+++ src/readwise2anki/__init__.py
@@ -1,5 +1,5 @@
 # import the main window object (mw) from aqt
-from aqt import mw
+from aqt import mw, AnkiQt
 
 # import the "show info" tool from utils.py
 from aqt.utils import showInfo, qconnect
@@ -7,22 +7,51 @@ from aqt.utils import showInfo, qconnect
 # import all of the Qt GUI library
 from aqt.qt import *
 
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
+from . import readwise
 
 
-def testFunction() -> None:
-    # get the number of cards in the current collection, which is stored in
-    # the main window
-    cardCount = mw.col.card_count()
-
-    # show a message box
-    showInfo("Card count: %d" % cardCount)
+class Highlights:
+    def __init__(self):
+        self.highlights = None
 
 
-# create a new menu item, "test"
-action = QAction("test", mw)
-# set it to call testFunction when it's clicked
-qconnect(action.triggered, testFunction)
+class AddonGUI(QMainWindow):
+    """Class that acts as the main GUI for the addon"""
+
+    def __init__(self, mw: AnkiQt) -> None:
+        # Calls the QWidget constructor and pass the main window as the parent
+        QWidget.__init__(self, mw)
+
+        # Update the window's title
+        self.setWindowTitle("Import from Readwise")
+
+        # Set the window's dimensions
+        self.setMinimumWidth(500)
+        self.setMinimumHeight(500)
+
+        # Display the GUI
+        self.show()
+
+        # Focus the window
+        self.setFocus()
+
+
+def menu() -> None:
+    """Display the menu to import from Readwise"""
+    # sel = readwise.fetch_from_export_api()
+
+    # showInfo(data[0][0])
+    AddonGUI(mw)
+
+
+# TODO: get highlights from readwise
+
+
+# create a new menu item, "Readwise Import"
+action = QAction("Import from Readwise", mw)
+
+# set it to call menu when it's clicked
+qconnect(action.triggered, menu)
+
 # and add it to the tools menu
 mw.form.menuTools.addAction(action)
--- src/readwise2anki/readwise.py
+++ src/readwise2anki/readwise.py
@@ -5,42 +5,74 @@ import pprint
 import requests
 from decouple import config  # type: ignore
 
-token = config("API_KEY")
 
+class Readwise:
+    def __init__(self):
+        self.token = config("API_KEY")
+        self.books = None
+        self.highlights = None
 
-def fetch_from_export_api(updated_after=None):
-    """Retrieve all highlights from the user's Readwise account. This is based off the example code given at: https://readwise.io/api_deets"""
-    full_data = []
-    next_page_cursor = None
-    while True:
-        params = {}
-        if next_page_cursor:
-            params["pageCursor"] = next_page_cursor
-        if updated_after:
-            params["updatedAfter"] = updated_after
-        print("Making export api request with params " + str(params) + "...")
-        response = requests.get(
-            url="https://readwise.io/api/v2/export/",
-            params=params,
-            headers={"Authorization": f"Token {token}"},
-            verify=True,
-            timeout=20,
-        )
-        full_data.extend(response.json()["results"])
-        next_page_cursor = response.json().get("nextPageCursor")
-        if not next_page_cursor:
-            break
-    return full_data
+    def fetch_from_export_api(self, updated_after=None) -> None:
+        """Retrieve all highlights from the user's Readwise account. This is based off the example code given at: https://readwise.io/api_deets"""
+        full_data = []
+        next_page_cursor = None
+        while True:
+            params = {}
+            if next_page_cursor:
+                params["pageCursor"] = next_page_cursor
+            if updated_after:
+                params["updatedAfter"] = updated_after
+            print("Making export api request with params " + str(params) + "...")
+            response = requests.get(
+                url="https://readwise.io/api/v2/export/",
+                params=params,
+                headers={"Authorization": f"Token {self.token}"},
+                verify=True,
+                timeout=20,
+            )
+
+            full_data.extend(response.json()["results"])
+            next_page_cursor = response.json().get("nextPageCursor")
+            if not next_page_cursor:
+                break
+
+        self.books = full_data
+
+    def get_info(self):
+        """Get all highlights, data, and tags from the data/"""
+        books = []
+        if self.books is not None:
+            all_books = self.books
+
+            # print highlights
+            for book in all_books:
+                if book["highlights"]:
+                    print(book["title"])
+                    if book["book_tags"] != []:
+                        print(book["book_tags"])
+
+                    books.append(book["title"])
+                    for highlight in book["highlights"]:
+                        print(f"\t {highlight['text']} \n")
+                        if highlight["tags"] != []:
+                            print(highlight["tags"])
+
+                    print("\n\n\n")
+
+        else:
+            print("You need to retrieve highlights from the API first")
 
 
 def main():
     """Main function used for testing"""
     # Get all of a user's books/highlights from all time
-    all_data = fetch_from_export_api()
+    account = Readwise()
+    account.fetch_from_export_api()
+    account.get_info()
 
     # Pretty print data
     pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(all_data)
+    # pp.pprint(account.books)
 
     # TODO: Implement this after you properly create cards from all data
     # # Later, if you want to get new highlights updated since your last fetch of allData, do this.
